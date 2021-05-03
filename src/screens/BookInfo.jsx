b025c231f92ef6f3f91f71f13541df470ccf344b
ansarali41 <ansar.ict14@gmail.com>
2021-05-04 02:09:17 +0600

    first commit

--- src/screens/BookInfo.jsx
+++ src/screens/BookInfo.jsx
@@ -1,86 +1,116 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import { useParams,Link } from 'react-router-dom';
-import { getOneBook } from '../services/books';
+import React from "react";
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import { getOneBook } from "../services/books";
 import ReactStars from "react-rating-stars-component";
 
-
-
 const BookInfo = (props) => {
-  const [book, setBook] = useState(null);
-  const { id } = useParams();
-  const { handleDelete } = props;
+    const [book, setBook] = useState(null);
+    const { id } = useParams();
+    const { handleDelete } = props;
 
-  
+    useEffect(() => {
+        const fetchBook = async () => {
+            const bookData = await getOneBook(id);
+            setBook(bookData);
+        };
+        fetchBook();
+    }, [id]);
 
-  useEffect(() => {
-    const fetchBook = async () => {
-      const bookData = await getOneBook(id);
-      setBook(bookData);
-    }
-    fetchBook();
-  }, [id])
+    return (
+        <div className="container-book-info pt-4">
+            <div className="row d-flex justify-content-center align-items-start">
+                <div className="col-11 col-sm-8 col-md-8 customize-book-info-card">
+                    <div className="row d-flex justify-content-center align-items-center">
+                        <div className="col-11 col-sm-11 col-md-6 mt-2">
+                            <div className="book-div">
+                                <span className="instock">In stock</span>
+                                <img
+                                    src={book?.image_url}
+                                    alt=""
+                                    className="book-info-image"
+                                />
+                                <button className="btn btn-primary mt-2 customize-btn">
+                                    <Link
+                                        to={`/books/${book?.id}/edit`}
+                                        className="customize-link"
+                                    >
+                                        Customize{" "}
+                                    </Link>
+                                </button>
 
- 
+                                <button
+                                    className="btn btn-danger delete-book-btn mt-2"
+                                    onClick={() => handleDelete(book.id)}
+                                >
+                                    Delete
+                                </button>
+                            </div>
+                        </div>
 
-  return (
-    <div className='book-info'>
-      <div className='upper-container'>
-        <div className='info-left-container'>
-          <div className='book-div'> <img src={book?.image_url} alt='' className='book-info-image' />
-          <Link to={`/books/${book?.id}/edit`}><span className='customize'>Customize</span></Link>
-            <span className='delete' onClick={()=>handleDelete(book.id)}>Delete</span>
+                        <div className="col-11 col-sm-11 col-md-6 mt-2 card-text-style ">
+                            <div className="book-info-text">
+                                <p>
+                                    <span>Name:</span> {book?.title}
+                                </p>
+                                <p>
+                                    <span>Condition:</span> {book?.condition}
+                                </p>
+                                <p>
+                                    <span>ISBN: </span>
+                                    {book?.isbn}
+                                </p>
+                                <p>
+                                    <span>Rental Price: </span> {book?.price}$
+                                </p>
+                                <p>
+                                    <span>Author: </span>
+                                    {book?.author_name}
+                                </p>
+                                <p>Free 2 Day Shipping</p>
+                                <p>Arrive 01.05.2021</p>
+
+                                <div className="d-flex align-items-center">
+                                    <p> Book Overall Rating: {book?.rating} </p>
+                                    <span className="reviews-star">
+                                        <ReactStars
+                                            className="due"
+                                            value={1}
+                                            edit={false}
+                                            count={1}
+                                            size={24}
+                                            activeColor="#ffd700"
+                                        />
+                                    </span>
+                                </div>
+                            </div>
+
+                            <div className="text-center">
+                                <Link to={`/books/${id}/rents`}>
+                                    <button className="btn btn-primary rent-btn">
+                                        RENT
+                                    </button>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="lower-container">
+                <div className="reviews">
+                    {/* <input type="textarea" />
+                    <img src="https://i.imgur.com/opaqxyG.png" alt="" /> */}
+                </div>
+                <div className="photo">
+                    <img
+                        className="img-fluid"
+                        src="https://i.imgur.com/t0UsMu4.png"
+                        alt=""
+                    />
+                </div>
             </div>
         </div>
-        <div className='info-middle-container'>
-          <div className='upper-inner-container'><p>{book?.title}</p></div>
-          <div className='lower-inner-container'>
-            <div className='left-lower-inner-container'>
-              <p><span>Condition:</span> {book?.condition}</p>
-              <p><span>ISBN: </span>{book?.isbn}</p>
-              <p><span>Rental Price: </span> {book?.price}$</p>
-              <p><span>Author: </span>{book?.author_name}</p>
-             
-            </div>
-            <div className='right-lower-inner-container'>
-              <div className='instock'><p>In stock</p></div>
-            </div>
-          </div>
-  
-        </div>
-        <div className='info-right-container'>
-          <div className='ad overall-rating'>
-          <h3> Book Overall Rating: {book?.rating}</h3>
-            
-            <ReactStars
-              className="due"
-              value={1}
-              edit={false}
-              count={1}
-              size={24}
-              activeColor="#ffd700"
-            />
-            </div>
-          <div className='ad'>
-            <p>Free 2 Day Shipping</p>
-            <p>Arrive 01.05.2021</p>
-          </div>
-          <div className='btn-rent'>
-            <Link to={`/books/${id}/rents`}><p>RENT</p></Link>
-          </div>
-        </div>
-      </div>
-      <div className='lower-container'>
-        <div className='reviews'>
-          <input type='textarea' />
-          <img src='https://i.imgur.com/opaqxyG.png' alt=''/>
-        </div>
-        <div className='photo'>
-          <img src='https://i.imgur.com/t0UsMu4.png' alt=''/>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default BookInfo;
--- src/screens/Catalog.jsx
+++ src/screens/Catalog.jsx
@@ -1,7 +1,9 @@
 import React, { useState }from 'react';
 import { Link } from 'react-router-dom';
+import Fuse from 'fuse.js'
+
 export default function Catalog(props) {
-  const { books} = props;
+  let { books} = props;//changed const to let
   const {handleFilters} = props;
   const [formData, setFormData] = useState({
     title: '',
@@ -20,13 +22,29 @@ export default function Catalog(props) {
       [name]: value
     }))
   }
+
+  //fuse js search added 
+  const [data, setData] = useState(books);
+  const [query,setQuery] = useState('')
+  const fuse = new Fuse(data, {
+    keys: ["title"],
+    includeScore:true
+  });
+  let results = fuse.search(query);
+  books = query? results.map(result => result.item) : books;
+  console.log(books.length);
+
+  function handleSearch(e) {
+    setQuery(e.target.value)
+  }
+  
+
   return (
     <div className='books-container'>
       
-
       <div className="left-container filters">
         
-        <div className='wrapper'>
+        <div className='wrapper '>
         <form onSubmit={(e) => {
             e.preventDefault();
             handleFilters(formData)
@@ -42,8 +60,8 @@ export default function Catalog(props) {
             />
           </label>
           <label>Condition:
-            <select className="form-select" name='condition' onChange={handleChange}>
-              <option selected>Open this select menu</option>
+            <select className="form-select form-select-option" name='condition' onChange={handleChange}>
+              <option defaultValue>Select Option</option>
               <option value="used">New</option>
               <option value="new">Used</option>
             </select>
@@ -77,26 +95,34 @@ export default function Catalog(props) {
      
           <input type="submit" value="Filter" name="submit" id="submit" />
             </form>
-            </div>
+        </div>
 
 
       </div>
       <div className='middle-container'>
-        <input type ='search' placeholder="Search" className='search'></input>
-        <div className="list-container">
-        {books.map(book => (
-        <div key={book.id} className='book-name'>
-          
-          <Link to={`/books/${book?.id}`} id='link'>
-            <img src={book?.image_url} alt='' className='book-img'/>
-            <p className='book-title'>{book?.title}</p>
-          </Link>
-         
-        </div>
-      ))}
+        <input type='search' placeholder="Search by Title" className='search form-control'
+          value={query}
+          onChange={handleSearch}>
+          </input>
+        <div className="row list-container">
+            {books.length ? books.map(book => (
+            <div key={book.id} className='col-6 col-sm-3 col-md-3 mt-2 books-card-container'>
+              <div className="content1 single-books-container d-flex justify-content-center align-items-center pt-4">
+                <Link to={`/books/${book?.id}`} id='link' className="text-center">
+                  <img src={book?.image_url} alt='' className='book-img img-fluid'/>
+                  <p className='book-title'>{book?.title}</p>
+                </Link>
+            </div>
+            </div>
+          )):
+          <div>
+            <h4 className="text-center text-white">Results not found</h4>
+          </div>
+          }
+      
         </div>
       
-      <img src={`https://i.imgur.com/vt1GHgF.png`} alt='' className='grass-img'/>
+        <img src={`https://i.imgur.com/vt1GHgF.png`} alt='' className='grass-img'/>
       </div>
       <div className='right-container'>
         <img src={`https://i.imgur.com/QLJuTHR.png`} alt='' className='tree-image'/>
